6c037f9bc0b3a01358a5f2c648486c7b1b1f88a5
Claire Ryberg <claire.ryberg@accenture.com>
2023-08-18 16:20:13 +0000

    Test image push

--- app/page.js
+++ app/page.js
@@ -1,15 +1,15 @@
 import './page.scss'
+import heroImage from '../public/images/morning-mountain-forest__mobile.jpeg'
 
 export default function Home() {
   return (
     <main className="home-page">
       <img
-        src='https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-morning-mountain-forest-scenery-free-photo.jpg?w=1280&quality=70'
+        src={heroImage}
         alt=""
         className="home-page__hero"
-        // className={styles.vercelLogo}
-        // width={100}
-        // height={24}
+        width={100}
+        height={24}
         // priority
       />
       <h1>Homepage</h1>
